272ee173ea354ecd87007491753977c4a5316e25
Alireza Farahani <30633987+amfarahani@users.noreply.github.com>
2022-10-27 09:22:49 -0700

    Update App.java
    Added a line to the App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,6 +11,7 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
+        System.out.println("Ali added this line\n");
         System.out.println(new App().getMessage());
     }
 
